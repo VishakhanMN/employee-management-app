ee8f810ba70459c9b0afe0be08fa003a47438a9b
Vishakhan MN <32939385+VishakhanMN@users.noreply.github.com>
2026-08-11 12:46:29 +0530

    Create interfaces.ts

---
+++ src/app/core/interfaces/interfaces.ts
@@ -0,0 +1,16 @@
+export interface LoginCredentials{
+    username: string,
+    password: string
+}
+
+export interface loggedInEmployeeData {
+  employeeId: number
+  employeeName: string
+  contactNo: string
+  emailId: string
+  deptId: number
+  password: string
+  gender: string
+  role: string
+  createdDate: string
+}
