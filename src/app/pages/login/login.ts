66e0df57b2438859d37ceb2f7c34ff6c39e8a3cd
Vishakhan MN <32939385+VishakhanMN@users.noreply.github.com>
2026-08-11 12:42:50 +0530

    Add files via upload

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,24 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginCredentials } from '../../core/interfaces/interfaces';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { GlobalConstant } from '../../core/globalConstants/global.constant';
 
 @Component({
   selector: 'app-login',
   imports: [FormsModule],
+  providers: [HttpClient],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-  username = '';
-  password = '';
 
-  constructor(private router: Router) {}
+  private router = inject(Router);
+  private http = inject(HttpClient);
+
+  loginCred: LoginCredentials = {
+    username: '',
+    password: ''
+  }
 
   login(): void {
-    if (!this.username.trim() || !this.password) {
+    if (!this.loginCred.username.trim() || !this.loginCred.password) {
       return;
     }
 
-    this.router.navigate(['/admin/dashboard']);
+    this.http.post(environment.API_URL + 'login', this.loginCred).subscribe({
+      next: (res: any) => {
+        if (res.result = true) {
+          localStorage.setItem(GlobalConstant.LOGIN_LOCAL_KEY, JSON.stringify(res.data))
+          this.router.navigate(['/admin/dashboard']);
+        }
+        else {
+          alert("Wrong login credetnials")
+        }
+      },
+      error: (err: any) => {
+        alert("API Error")
+      }
+    }
+
+    )
+
+
   }
 }
