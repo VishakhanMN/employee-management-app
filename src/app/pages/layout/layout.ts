63b32be79e05146a87c4b2c576373cbca97e6da7
Vishakhan MN <32939385+VishakhanMN@users.noreply.github.com>
2026-08-11 12:42:03 +0530

    Add files via upload

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal, Signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { loggedInEmployeeData } from '../../core/interfaces/interfaces';
+import { GlobalConstant } from '../../core/globalConstants/global.constant';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet, CommonModule, FormsModule, RouterLink],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
-export class Layout {
+export class Layout implements OnInit {
+
+  isMenuOpened = signal<boolean>(true);
+  isUserMenuOpened = signal<boolean>(false);
+  loggedInUser!: loggedInEmployeeData;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    const userData = localStorage.getItem(GlobalConstant.LOGIN_LOCAL_KEY);
+    this.loggedInUser = userData ? JSON.parse(userData) : {} as loggedInEmployeeData;
+  }
+
+  toggleMenuIcon(): any {
+    this.isMenuOpened.update(value => !value)
+  }
+
+  toggleUserMenu(): void {
+    this.isUserMenuOpened.update(value => !value);
+  }
+
+  logout(): void {
+    localStorage.removeItem(GlobalConstant.LOGIN_LOCAL_KEY);
+    this.router.navigateByUrl('/login');
+  }
 
 }
